060b71be1af51ef83ed1a627c86b50a6f3935f8d
Duy DV <at160508@actvn.edu.vn>
2022-12-19 20:39:28 +0700

    commit 19/12

---
+++ app/Models/StoryModel.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace App\Models;
+
+use Spatie\Searchable\Searchable;
+use Spatie\Searchable\SearchResult;
+use Illuminate\Database\Eloquent\Model;
+
+class StoryModel extends Model implements Searchable
+{
+    // Ket noi database
+    public $table = "story";
+    public $timestamps = false;
+    public $fillable = [
+        'id',
+        'story_name',
+        'description',
+        'status'
+    ];
+
+    public function getSearchResult(): SearchResult
+    {
+
+        $url = route('all-story', $this->id);
+
+        return new SearchResult(
+            $this,
+            $this->story_name,
+            $url
+        );
+    }
+    public function Category()
+    {
+        return $this->belongsToMany('App\Models\CategoryModel','story_category' ,'story_id', 'category_id');
+    }
+
+}
